ff13ae8be1a715870df66cba941fdcd1d5f8e26a
ImJoke <55929550+ImJoke@users.noreply.github.com>
2020-12-17 03:19:19 +0700

    Update module.py

--- module.py
+++ module.py
@@ -1,8 +1,8 @@
-from pencaridari import *
+# from pencaridari import *
 
-sin_α = input("Masukkan nilai sin α = ")
-cos_α = input("Masukkan nilai cos α = ")
-tan_α = input("Masukkan nilai tan α = ")
-cot_α = input("Masukkan nilai cot α = ")
-sec_α = input("Masukkan nilai sec α = ")
-csc_α = input("Masukkan nilai csc α = ")
+# sin_α = input("Masukkan nilai sin α = ")
+# cos_α = input("Masukkan nilai cos α = ")
+# tan_α = input("Masukkan nilai tan α = ")
+# cot_α = input("Masukkan nilai cot α = ")
+# sec_α = input("Masukkan nilai sec α = ")
+# csc_α = input("Masukkan nilai csc α = ")
